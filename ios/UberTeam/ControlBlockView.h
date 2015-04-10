d55af76214f9deaa4d370df568be7cac84ef7a8c
Jason Jiang <jasonj@uber.com>
2015-04-10 16:18:04 -0700

    Adding ios project

---
+++ ios/UberTeam/ControlBlockView.h
@@ -0,0 +1,13 @@
+//
+//  ControlBlockView.h
+//  UberTeam
+//
+//  Created by Jason Jiang on 4/10/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ControlBlockView : UIView
+
+@end
---
+++ ios/UberTeam/ControlBlockView.m
@@ -0,0 +1,21 @@
+//
+//  ControlBlockView.m
+//  UberTeam
+//
+//  Created by Jason Jiang on 4/10/15.
+//
+//
+
+#import "ControlBlockView.h"
+
+@implementation ControlBlockView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
