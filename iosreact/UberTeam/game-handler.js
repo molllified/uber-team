407979b2c8ff4fae3baa32dc8e5ceab970badcdf
jasonj <jasonj@uber.com>
2015-04-10 21:38:22 -0700

    Adding objects. They dont work

--- iosreact/UberTeam/game-handler.js
+++ iosreact/UberTeam/game-handler.js
@@ -1,9 +1,9 @@
 var GameModel = require('game-model').GameModel;
 
-var GameHandler = function (playerId) {
+var GameHandler = function (userId) {
   var gameState = new GameModel();
-  var eventLoop = this.startEventLoop(100);
-  var playerId = playerId;
+  var eventLoop = this.startEventLoop(15);
+  var userId = userId;
 }
 
 GameHandler.prototype.startEventLoop = function(interval) {
@@ -12,13 +12,13 @@ GameHandler.prototype.startEventLoop = function(interval) {
 
 GameHandler.prototype.update = function() {
   var currTime = new Date().getTime();
-  var myInstructions = this.getPlayerInstructions();
+  var myInstructions = this.getuserInstructions();
 }
 
 // For when server tells me to update my state
 GameHandler.prototype.handleStateUpdate = function(newState) {
   this.gameState = newState;
-  this.dispatchUIChanges();
+  this.dispatchUIChanges(); //and transitions
 }
 
 // For when my own actions caused my state to change
@@ -26,4 +26,4 @@ GameHandler.prototype.handleStateChange = function(newState) {
   // todo
 }
 
-GameHandler.prototype.getPlayerInstructions = function() 
+GameHandler.prototype.getuserInstructions = function() 
--- iosreact/UberTeam/game-model.js
+++ iosreact/UberTeam/game-model.js
@@ -1,21 +1,17 @@
 'use strict';
 
 var GameModel = function() {
-  var currScore: 0;
-  var players: [];
-  var timeToDest: 0;
+  var currScore= 0;
+  var playerInstructions= {};
+  var timeToDest= 0;
 };
 
-var Player = function () {
-  var instructions: [];
-  var uuid: '';
-};
-
-var Instruction = function () {
-  var type: null;
-  var expireTime: null;
-  var startTime: null;
-  var started: false;
+var PlayerInstruction = function () {
+  var type= null;
+  var expireTime= null;
+  var startTime= null;
+  var started= false;
+  var data= null;
 }
 
 module.exports = {
