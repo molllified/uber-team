01618492b301491bdec58bf836a7a9e814231cc9
jasonj <jasonj@uber.com>
2015-04-10 20:30:47 -0700

    updated readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Awesome hackathon project
 
 Setup:
 pip install -r requirements.txt
+
+# Installing react
+npm install -g react-native-cli@0.1.3
--- iosreact/UberTeam/game-handler.js
+++ iosreact/UberTeam/game-handler.js
@@ -1,1 +1,29 @@
 var GameModel = require('game-model').GameModel;
+
+var GameHandler = function (playerId) {
+  var gameState = new GameModel();
+  var eventLoop = this.startEventLoop(100);
+  var playerId = playerId;
+}
+
+GameHandler.prototype.startEventLoop = function(interval) {
+  return setInterval(this.update, interval);
+}
+
+GameHandler.prototype.update = function() {
+  var currTime = new Date().getTime();
+  var myInstructions = this.getPlayerInstructions();
+}
+
+// For when server tells me to update my state
+GameHandler.prototype.handleStateUpdate = function(newState) {
+  this.gameState = newState;
+  this.dispatchUIChanges();
+}
+
+// For when my own actions caused my state to change
+GameHandler.prototype.handleStateChange = function(newState) {
+  // todo
+}
+
+GameHandler.prototype.getPlayerInstructions = function() 
